2e07c68147db0932c3c8c48527b654832e83440c
vinayjujjuri <85351200+vinayjujjuri@users.noreply.github.com>
2024-04-22 13:00:32 +0530

    staarted resume content

--- my-website/src/components/resume/resume.tsx
+++ my-website/src/components/resume/resume.tsx
@@ -1,11 +1,91 @@
 import styles from './resume.module.css';
+import SchoolIcon from '@mui/icons-material/School';
+import HomeRepairServiceIcon from '@mui/icons-material/HomeRepairService';
 
 export function ResumeComponent() {
+
+    const educationCard = () => {
+        return (
+            <div className={styles.oddCardContainer}>
+                <div className={styles.year}>
+                    2017-2021
+                </div>
+                <div className={styles.cardContent}>
+                    B.Tech in ECE - SBIT,
+                </div>
+                <div className={styles.cardSubContent}>
+                    Khammam, TS
+                </div>
+            </div>
+        )
+    };
+
+    const eeperienceCard = () => {
+        return (
+            <>
+            <div className={styles.evenCardContainer}>
+                <div className={styles.year}>
+                    2023-2024
+                </div>
+                <div className={styles.cardContent}>
+                    Associate Engineer,
+                </div>
+                <div className={styles.cardSubContent}>
+                    Innominds, HYD
+                </div>
+            </div>
+            <div className={styles.oddCardContainer}>
+                <div className={styles.year}>
+                    2021-2023
+                </div>
+                <div className={styles.cardContent}>
+                    Trainee,
+                </div>
+                <div className={styles.cardSubContent}>
+                    Innominds, HYD
+                </div>
+            </div>
+            </>
+        )
+    }
+
+    const educationContainer = () => {
+        return (
+            <div className={styles.educationContainer}>
+                <div className={styles.topContainer}>
+                    <SchoolIcon/>
+                    <div>
+                        Education
+                    </div>
+                </div>
+                {educationCard()}
+            </div>
+        )
+    };
+
+    const experinceContainer = () => {
+        return (
+            <div className={styles.educationContainer}>
+                <div className={styles.topContainer}>
+                    <SchoolIcon/>
+                    <div>
+                        Experience
+                    </div>
+                </div>
+                {eeperienceCard()}
+            </div>
+        )
+    };
+
     return (
-        <div>
+        <div className={styles.mainContainer}>
             <div className={styles.headingContainer}>
                 <div className={styles.about}>Resume</div>
                 <div className={styles.line}></div>
+            </div>
+            <div className={styles.subContainer}>
+                {educationContainer()}
+                {experinceContainer()}
             </div>
         </div>
     )
